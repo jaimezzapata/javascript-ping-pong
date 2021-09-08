b668d28013f47722830760266f59f5bb43c6f5de
Jaime Zapata <zapataval2304@gmail.com>
2021-09-08 12:42:00 -0500

    Se cambian las variables del JS

--- js/app.js
+++ js/app.js
@@ -2,139 +2,115 @@
 var ps = 15;
 
 function nfp(urpx) {
-    return Number(urpx.replace("px", ""))
+  return Number(urpx.replace("px", ""));
 }
 
-var r = document.getElementById('right');
-var l = document.getElementById('left');
-var b = document.getElementById('ball');
+var juadorDerecha = document.getElementById("right");
+var izquierda = document.getElementById("left");
+var pelota = document.getElementById("ball");
 
-var rscore = document.getElementById('scoreleft');
-var lscore = document.getElementById('scoreright');
-var ogoal = document.getElementById('goal');
+var puntuacionDerecha = document.getElementById("scoreleft");
+var puntuacionIzquierda = document.getElementById("scoreright");
+var ogoal = document.getElementById("goal");
 
-var w = window.innerWidth;
-var h = window.innerHeight;
+var ancho = window.innerWidth;
+var alto = window.innerHeight;
 
-var map = []; // Or you could call it "key"
-onkeydown = onkeyup = function(e) {
-    e = e || event; // to deal with IE
-    map[e.keyCode] = e.type == 'keydown';
-    /*insert conditional here*/
-}
+var map = [];
+onkeydown = onkeyup = function (e) {
+  map[e.keyCode] = e.type == "keydown";
+};
 
 function keydown() {
-    //if key was up arrow
-    if (map[40]) {
-        if (nfp(r.style.top) + ps > h - 200)
-            r.style.top = h - 200 + "px";
-        else
-            r.style.top = nfp(r.style.top) + ps + "px";
+  if (map[40]) {
+    if (nfp(juadorDerecha.style.top) + ps > alto - 200) {
+      juadorDerecha.style.top = alto - 200 + "px";
+    } else {
+      juadorDerecha.style.top = nfp(juadorDerecha.style.top) + ps + "px";
     }
+  }
 
-
-
-    //if key was down arrow
-    else if (map[38]) {
-        if (nfp(r.style.top) - ps < 0)
-            r.style.top = 0 + "px";
-        else
-            r.style.top = nfp(r.style.top) - ps + "px";
+  else if (map[38]) {
+    if (nfp(juadorDerecha.style.top) - ps < 0) {
+      juadorDerecha.style.top = 0 + "px";
+    } else {
+      juadorDerecha.style.top = nfp(juadorDerecha.style.top) - ps + "px";
     }
+  }
 
-
-    //if key was s
-    if (map[83]) {
-        if (nfp(l.style.top) + ps > h - 200)
-            l.style.top = h - 200 + "px";
-        else
-            l.style.top = nfp(l.style.top) + ps + "px";
+  if (map[83]) {
+    if (nfp(izquierda.style.top) + ps > alto - 200) {
+      izquierda.style.top = alto - 200 + "px";
+    } else {
+      izquierda.style.top = nfp(izquierda.style.top) + ps + "px";
     }
-
-    //if key was w
-    else if (map[87]) {
-        if (nfp(l.style.top) - ps < 0)
-            l.style.top = 0 + "px";
-        else
-            l.style.top = nfp(l.style.top) - ps + "px";
+  } else if (map[87]) {
+    if (nfp(izquierda.style.top) - ps < 0) {
+      izquierda.style.top = 0 + "px";
+    } else {
+      izquierda.style.top = nfp(izquierda.style.top) - ps + "px";
     }
-
-    //40 down, 38 up
-    //w 87,s 83
+  }
 }
-
 
 var speedx = 3,
-    speedy = 1;
+  speedy = 1;
 var balltime = 1;
-b.style.left = w / 2 + "px";
+pelota.style.left = ancho / 2 + "px";
 
 function ball() {
-    b.style.left = nfp(b.style.left) + speedx + "px";
-    b.style.top = nfp(b.style.top) + speedy + "px";
+  pelota.style.left = nfp(pelota.style.left) + speedx + "px";
+  pelota.style.top = nfp(pelota.style.top) + speedy + "px";
 }
-
-
-
 
 function moveball() {
-    ball();
+  ball();
 
-    //remove overflow y
-    if (h < nfp(b.style.top) + 20 || nfp(b.style.top) < 0) {
-        speedy *= -1;
-    }
+  if (alto < nfp(pelota.style.top) + 20 || nfp(pelota.style.top) < 0) {
+    speedy *= -1;
+  }
 
-    //overflow-x right
-    if (nfp(b.style.left) >= w - 50) {
-        if (nfp(r.style.top) <= nfp(b.style.top) + 20 && nfp(r.style.top) + 200 >= nfp(b.style.top)) {
-            speedx *= -1;
-        } else if (nfp(b.style.left) >= w - 20)
-            goal('left');
-    }
+  if (nfp(pelota.style.left) >= ancho - 50) {
+    if (
+      nfp(juadorDerecha.style.top) <= nfp(pelota.style.top) + 20 &&
+      nfp(juadorDerecha.style.top) + 200 >= nfp(pelota.style.top)
+    ) {
+      speedx *= -1;
+    } else if (nfp(pelota.style.left) >= ancho - 20) goal("left");
+  }
 
+  if (nfp(pelota.style.left) <= 30) {
+    if (
+      nfp(izquierda.style.top) <= nfp(pelota.style.top) + 20 &&
+      nfp(izquierda.style.top) + 200 >= nfp(pelota.style.top)
+    ) {
+      speedx *= -1;
+    } else if (nfp(pelota.style.left) <= 0) goal("right");
+  }
 
-
-
-    //remove overflow x in left ir get the goal in left
-    if (nfp(b.style.left) <= 30) {
-        if (nfp(l.style.top) <= nfp(b.style.top) + 20 && nfp(l.style.top) + 200 >= nfp(b.style.top)) {
-            speedx *= -1;
-        } else if (nfp(b.style.left) <= 0)
-            goal('right');
-    }
-
-
-
-    setTimeout(function() {
-        moveball()
-    }, balltime);
+  setTimeout(function () {
+    moveball();
+  }, balltime);
 }
 
-
-
-
-setInterval(function() {
-    keydown();
+setInterval(function () {
+  keydown();
 }, 10);
 moveball();
 
 function goal(pos) {
+  ogoal.style.color = "white";
 
-    ogoal.style.color = "white";
+  setTimeout(function () {
+    ogoal.style.color = "black";
+  }, 10);
 
-    setTimeout(function() {
-        ogoal.style.color = "black"
-    }, 1000);
+  if (pos == "left") {
+    puntuacionDerecha.innerHTML = Number(puntuacionDerecha.innerHTML) + 1;
+  } else {
+    puntuacionIzquierda.innerHTML = Number(puntuacionIzquierda.innerHTML) + 1;
+  }
 
-    if (pos == "left")
-        rscore.innerHTML = Number(rscore.innerHTML) + 1;
-    else
-        lscore.innerHTML = Number(lscore.innerHTML) + 1;
-
-
-    speedx *= -1;
-    b.style.left = w / 2 + "px";
-
-
+  speedx *= -1;
+  pelota.style.left = ancho / 2 + "px";
 }
