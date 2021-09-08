b71bd930572e20d8be1561dbc09ae338ed4a13f9
Jaime Zapata <zapataval2304@gmail.com>
2021-09-08 11:58:38 -0500

    Se crea el archivo JS y se declaran las variables

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var r = document.getElementById('right');
+var l = document.getElementById('left');
+var b = document.getElementById('ball');
+
+var rscore = document.getElementById('scoreleft');
+var lscore = document.getElementById('scoreright');
+var ogoal = document.getElementById('goal');
+
+var w = window.innerWidth;
+var h = window.innerHeight;
